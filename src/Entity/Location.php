be1603681fb8feaadac5a3676751560249d1d92c
um49878 <um49878@zut.edu.pl>
2023-11-20 18:12:55 +0100

    Laboratorium 5(E) - Security

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+
 #[ORM\Entity(repositoryClass: LocationRepository::class)]
 class Location
 {
